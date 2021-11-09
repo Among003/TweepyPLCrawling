48b9f491168ceb7ee035c674a6f9ce0e83b0c5c8
AdrianMainComp <among003@ucr.edu>
2021-11-08 22:09:51 -0800

    Saving to organized directories done

--- TweepyPLCrawler.py
+++ TweepyPLCrawler.py
@@ -1,6 +1,5 @@
 from tweepy import OAuthHandler, Stream, StreamListener 
-import json
-import os
+import json, os, sys
 #These are our keys generated with our development account application 
 
 consumer_key="OvtaSdGdSFYG2enOytwNOtdea"
@@ -11,6 +10,13 @@ access_token_secret="HoiZyEXwrBUIHDKiI3U1VXEZ1uvBgRj6VfTuYE6u44M5X"
 #Current simple filter list
 #You can have multiple filters/hashtags 
 filterList = ['#ARSVIL']
+dataDirectory = "JSONData"
+
+
+def saveToJsonFile(jsonObj):
+    print("TODO")
+
+
 
 class StreamListener(StreamListener):
     #This is where the main functionality is written.  The listener has 2 functions built in that act 
@@ -23,11 +29,24 @@ class StreamListener(StreamListener):
         print(status)
 
 if __name__ == '__main__':
-    l = StdOutListener()
+
+    if len(sys.argv) < 2:                  #Need to pass in a filter for a hastag
+        print("Error, pass in hashtag")    
+        quit(1)                           
+
+    if dataDirectory not in os.listdir("."):              #folder to be used to store data.  Everything will be saved locally
+        print("Data folder not found, creating 'data directory as '", dataDirectory,"'...")
+        os.mkdir(dataDirectory)
+
+    if sys.argv[1] not in os.listdir(dataDirectory + "/"):
+        print("Making folder for ",sys.argv[1],"...")
+        os.mkdir(dataDirectory + "/" + sys.argv[1]) 
+
+    l = StreamListener()
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     stream = Stream(auth, l)
-    stream.filter(track=filterList)
+    #stream.filter(track=filterList)
 
 
 
