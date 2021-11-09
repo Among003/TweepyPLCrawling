8ccd6b7dca5e291f8f173e43058ecf134eda811e
AdrianMainComp <among003@ucr.edu>
2021-11-08 22:50:07 -0800

    Finished saving collected Tweets into files

--- TweepyPLCrawler.py
+++ TweepyPLCrawler.py
@@ -8,21 +8,25 @@ access_token="4808950815-vd7kZ2tQbHfM6KdIBtrKejEbU4rO3i8usY2mzqR"
 access_token_secret="HoiZyEXwrBUIHDKiI3U1VXEZ1uvBgRj6VfTuYE6u44M5X"
 
 #Current simple filter list
-#You can have multiple filters/hashtags 
-filterList = ['#ARSVIL']
+#You can have multiple filters/hashtags.  Add some default ones here, or pass it in through argv 
+filterList = []
 dataDirectory = "JSONData"
 
 
 def saveToJsonFile(jsonObj):
-    print("TODO")
-
+    outfile = open(dataDirectory + "/" + sys.argv[1] + "/" +jsonObj.get("id_str") + ".json", 'w')
+    print("Saving tweet: ", jsonObj.get("id_str"))
+    json.dump(jsonObj, outfile, indent = 5)
+    outfile.close()
 
 
 class StreamListener(StreamListener):
     #This is where the main functionality is written.  The listener has 2 functions built in that act 
     #depending on successful tweet pull or failure.
     def on_data(self, data):
-        print(data)
+        
+        dataJson = json.loads(data)
+        saveToJsonFile(dataJson) 
         return True
 
     def on_error(self, status):
@@ -41,18 +45,19 @@ if __name__ == '__main__':
     if sys.argv[1] not in os.listdir(dataDirectory + "/"):
         print("Making folder for ",sys.argv[1],"...")
         os.mkdir(dataDirectory + "/" + sys.argv[1]) 
-
+    
+    filterList.append(sys.argv[1])
     l = StreamListener()
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     stream = Stream(auth, l)
-    #stream.filter(track=filterList)
+    stream.filter(track=filterList)
 
 
 
 #PLANNED FUNCTIONALITY
-#1. Passable hashtag through ARGV
-#2. Creation of directories based on input hashtag
-#3. Saving of Tweets into JSON
-#     3.1 Selection of only necessary data from JSON? Or save whole Json file?
-#4. Ensuring tweets are original?
+#1. Passable hashtag through ARGV DONE
+#2. Creation of directories based on input hashtag  DONE
+#3. Saving of Tweets into JSON  DONE
+#     3.1 Selection of only necessary data from JSON? Or save whole Json file? ???? TODO???
+#4. Ensuring tweets are original?  TODO
